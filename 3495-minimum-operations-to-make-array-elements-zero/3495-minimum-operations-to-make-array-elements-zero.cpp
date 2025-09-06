621330af6b464ae0587262cbb813fcabeb52f62d
Sidharth R K <161910602+sidharth756@users.noreply.github.com>
2025-09-06 23:10:50 +0530

    Time: 113 ms (27.54%), Space: 207.4 MB (15.94%) - LeetHub

---
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minOperations(vector<vector<int>>& queries) {
+        long long operations=0;
+    
+
+        for(auto q: queries){
+            long long p=log(q[0])/log(4) + 1;
+            long long l=pow(4,p);
+
+            long long op=(q[1]-q[0] +1)*p;
+
+            while(q[1]>=l){
+                op = op + q[1]-l+1;
+                l= l * 4;
+            }
+            operations = operations + (op+1)/2;
+
+        }
+        return operations;
+    }
+};

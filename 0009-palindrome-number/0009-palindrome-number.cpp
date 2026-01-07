afae28fdae5f63408ac576aec75de81daf3f2a08
Sidharth R K <161910602+sidharth756@users.noreply.github.com>
2026-01-08 00:50:37 +0530

    Time: 3 ms (46.34%), Space: 8.5 MB (91.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    int rem, copy;
+    if(x < 0)
+    {
+        return 0;
+    }
+    long long rev = 0;
+    copy = x;
+    while (x != 0) {
+        rem = x % 10;
+        rev = (rev * 10) + rem;
+        x = x / 10;
+    }
+    if (copy == rev) 
+    {
+        return 1;
+    } 
+    else
+        return 0;
+}
+};
